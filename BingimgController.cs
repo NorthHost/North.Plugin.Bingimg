5a0dc955c4e065362f83aa22470d472cd41b24a6
Redns <ljing431@163.com>
2023-03-22 19:27:45 +0800

    ✨ 初始化仓库

---
+++ BingimgController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace North.Plugin.Bingimg.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BingimgController : ControllerBase
+    {
+        /// <summary>
+        /// Bing 每日一图链接获取
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello, World";
+        }
+    }
+}
